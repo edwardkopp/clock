b927d029eeb0e71c41b87f1270fb96a1d1578ea6
Edward Kopp <edward@edwardkopp.com>
2024-12-05 00:32:57 -0500

    add cool transparency

--- ui/__init__.py
+++ ui/__init__.py
@@ -8,6 +8,7 @@ class App(_Tk):
         _Tk.__init__(self)
         self.title("Clock")
         self.attributes("-topmost", True)
+        self.attributes("-alpha", 0.6)
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.quit)
         self.clock = _Clock(self)
