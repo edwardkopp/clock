bd60b1d7fa5a65effb0e6c12ee297ed9f3ec1930
Edward Kopp <edward@edwardkopp.com>
2024-12-12 06:03:14 -0500

    set specific window size to prevent width changing on MacOS

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from customtkinter import CTk as _Tk, set_appearance_mode as _set_mode
+from customtkinter import CTk as _Tk, set_appearance_mode as _set_mode, BOTH as _BOTH
 from .clock import Clock as _Clock
 
 
@@ -7,12 +7,13 @@ class App(_Tk):
     def __init__(self):
         _Tk.__init__(self)
         self.title("Clock")
+        self.geometry("320x240")
         self.attributes("-topmost", True)
         self.attributes("-alpha", 0.7)
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.quit)
         self.clock = _Clock(self)
-        self.clock.pack()
+        self.clock.pack(fill=_BOTH, expand=True)
 
     def run(self):
         _set_mode("dark")
